d792d0f228c7f2a95e01db978ecedc64bd610d64
donggi <ldg6153@gmail.com>
2021-10-18 22:43:48 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyupbit
+
+#exchange API
+#Access Key와 Sercret Key를 사용해서 Upbit 객체를 생성합니다. 이는 웹페이지에서 로그인하는 것과 같습니다.
+
+access = "ispYBg7aHn906gd1XXWlclMzVCoO0IIreDXr2oCZ"          # 본인 값으로 변경
+secret = "uPzw9HonEVdWpjhlx5dFMxZ23q7v7Biwx5uSn9hq"          # 본인 값으로 변경
+upbit = pyupbit.Upbit(access, secret)
+
+#잔고 조회
+#get_balance 메서드는 입력받은 티커의 보유 수량 정보를 조회합니다.
+
+print('리플 : ', upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
+print('리플 : ', upbit.get_balance("KRW-BTC"))     # KRW-XRP 조회
+print('보유 원화 : ', upbit.get_balance("KRW"))         # 보유 현금 조회
